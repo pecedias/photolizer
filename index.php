3e1a931b1f589592675f2be0c1042beb7182eb8f
Paulo Dias <pc_dias@live.com>
2018-04-05 17:22:49 -0300

    add favicon

--- index.php
+++ index.php
@@ -34,6 +34,7 @@ $id = $_GET['id'];
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>PhotoLizer</title>
+    <link rel="icon" href="assets/img/eye-open.svg">
 
     <!-- Fontawesome -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
